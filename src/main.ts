939ffe4a117ac2f748096f4758b52d5075dcb720
chanhong-ji <jichanhong24@gmail.com>
2025-12-10 20:14:32 +0900

    chore: 테스트용 docker 파일 수정

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('🚀 Running in', process.env.NODE_ENV, 'mode');
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ExceptionFilter());
   app.use(cookieParser());
@@ -16,5 +18,6 @@ async function bootstrap() {
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
+  console.log(`🚀 Server is running on port ${process.env.PORT ?? 4000}`);
 }
 bootstrap();
